27bed058f3761addf0fbbab40aa813b5651e1d12
ehager77 <eric@isabel.io>
2019-03-24 15:48:29 -0700

    added concert-this

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ require('dotenv').config({ path: __dirname + '/.env' });
 var Movie = require("./axios-calls/movie.js");
 var Spotify = require('node-spotify-api');
 const keys = require('./keys.js');
+var request = require('request');
+var moment = require('moment');
 var fs = require("fs");
 
 // Create a new Movie object
@@ -18,16 +20,13 @@ var term = process.argv.slice(3).join(" ");
 if (search === "movie-this") {
   console.log("Searching for movie....");
   movie.findMovie(term);
-} else {
-  console.log("Error");
-};
-
-if (search === "spotify-this") {
+} else if (search === "spotify-this") {
   console.log("Searching for song....");
   spotifyThisSong(term);
-} else {
-  console.log("Error");
-}
+} else if (search === "concert-this") {
+  console.log("Searching for concerts....");
+  concertThis(term);
+} 
 
 function spotifyThisSong(song) {
   spotify.search({ type: 'track', query: song, limit: 1 }, function (error, data) {
@@ -45,7 +44,7 @@ function spotifyThisSong(song) {
         console.log("-----------------------");
       }
 
-      var songEntry =[
+      var songEntry = [
         "Artist: " + songData.artists[0].name,
         "Song: " + songData.name,
         "Album: " + songData.album.name
@@ -63,7 +62,40 @@ function spotifyThisSong(song) {
 
 }
 
+function concertThis(concert) {
 
+    var queryUrl = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
+
+    request(queryUrl, function(error, response, body) {
+
+      if (!error && response.statusCode === 200) {
+    
+        var JS = JSON.parse(body);
+        for (i = 0; i < JS.length; i++)
+        {
+          var dTime = JS[i].datetime;
+            var month = dTime.substring(5,7);
+            var year = dTime.substring(0,4);
+            var day = dTime.substring(8,10);
+            var dateForm = month + "/" + day + "/" + year
+      
+          console.log("\n---------------------------------------------------\n");
+    
+            
+          console.log("Date: " + dateForm);
+          console.log("Name: " + JS[i].venue.name);
+          console.log("City: " + JS[i].venue.city);
+          if (JS[i].venue.region !== "")
+          {
+            console.log("Country: " + JS[i].venue.region);
+          }
+          console.log("Country: " + JS[i].venue.country);
+          console.log("\n---------------------------------------------------\n");
+    
+        }
+      }
+    });
+    }
 
 
 
